dc178f017080be27f7d83caca4520a86f13d6dde
Angel <angel-ortega27@hotmail.com>
2022-09-09 10:01:03 -0700

    add rencerBoard function to properly display game

--- tic-tac-toe-logic.js
+++ tic-tac-toe-logic.js
@@ -2,17 +2,44 @@ const Gameboard = (function() {
 
    const game = document.querySelectorAll("[class^='board-square']");
    const start = document.getElementById('startGame');
+   const gameboard = document.querySelector(".board");
+   let gameButtons = [];
+   let buttonID = '';
 
    function renderGameboard() {
+    if(gameboard.style.display === 'grid') {
+        gameboard.style.dispay = 'none';
+    } else {
+        gameboard.style.display = 'grid';
+    }
     for (let i = 0; i < game.length; i++) {
-        const btn = document.createElement('button');
-        btn.id = 'mark';
+        let btn = document.createElement('button');
+        btn.id = 'mark' + i;
+        btn.classList = 'before'
         game[i].appendChild(btn);
         start.disabled = true;
+        game[i].addEventListener('click', function(){
+            addMarker(btn);
+        })
     };
+    
    };
-   
+   function addMarker(button) {
+    let mark = document.getElementById(`${button.id}`);
+    mark.classList = 'mark'
+    mark.textContent = 'X';
+   }
    return {
     renderGameboard
    };
 })();
+
+function CreatePlayer(firstName, playerWins) {
+    return {
+        firstName: firstName,
+        playerWins:  playerWins,
+        getPlayerInfo() {
+            return firstName + playerWins;
+        },
+    };
+}
