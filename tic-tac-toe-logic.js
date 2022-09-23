e6dbc3b759e5faa0d098b91f0588187c5a5daa63
Angel <angel-ortega27@hotmail.com>
2022-09-23 16:16:19 -0700

    update vertical checks and upadated diagonal checks

--- tic-tac-toe-logic.js
+++ tic-tac-toe-logic.js
@@ -1,152 +1,178 @@
 const Gameboard = (function() {
-   const game = document.querySelectorAll("[class^='board-square']");
-   const start = document.getElementById('startGame');
-   const gameboard = document.querySelector(".board");
-   const gameStatus = document.getElementById('game-status');
-   const winningPlayer = document.getElementById('winner');
-   
-
-   let turn = true;
-   let gameButtons = [];
-   function renderGameboard() {
-    if(gameboard.style.display === 'grid') {
-        gameboard.style.display = 'none';
-    } else {
-        gameboard.style.display = 'grid';
-    }
-    for (let i = 0; i < game.length; i++) {
-        let btn = document.createElement('button');
-        btn.id = 'mark' + i;
-        btn.classList = 'before'
-        game[i].appendChild(btn);
-        start.disabled = true;
-        gameButtons.push(btn);
-        game[i].addEventListener('click', function(){
-            addMarker(btn)
-        })
+    const game = document.querySelectorAll("[class^='board-square']");
+    const start = document.getElementById('startGame');
+    const gameboard = document.querySelector(".board");
+    const gameStatus = document.getElementById('game-status');
+    const winningPlayer = document.getElementById('winner');
+    
+ 
+    let turn = true;
+    let gameButtons = [];
+    function renderGameboard() {
+     if(gameboard.style.display === 'grid') {
+         gameboard.style.display = 'none';
+     } else {
+         gameboard.style.display = 'grid';
+     }
+     for (let i = 0; i < game.length; i++) {
+         let btn = document.createElement('button');
+         btn.id = 'mark' + i;
+         btn.classList = 'before'
+         game[i].appendChild(btn);
+         start.disabled = true;
+         gameButtons.push(btn);
+         game[i].addEventListener('click', function(){
+             addMarker(btn)
+         })
+     };
     };
-   };
-   function addMarker(button) {
-    const marker = document.getElementById(`${button.id}`)
-    gameStatus.innerHTML = 'Current players turn: ' + currentPlayer();
-
-    if(marker.firstChild) {
-        return;
+    function addMarker(button) {
+     const marker = document.getElementById(`${button.id}`)
+     gameStatus.innerHTML = 'Current players turn: ' + currentPlayer();
+ 
+     if(marker.firstChild) {
+         return;
+     }
+     else {
+         if(Players.getPlayerName.called == true) {
+             if(turn){
+                 turn = false;
+                 playerOneMarks(button), checkHorizontal(), checkDiagonals(), checkVertical();
+             }
+            else {
+                turn = true;
+                playerTwoMarks(button), checkHorizontal(), checkDiagonals(), checkVertical();
+             }
+         }
+         if(checkHorizontal() == true || checkDiagonals() == true || checkVertical() == true){
+             Players.playerOneWins();
+             gameWinner(Players.getPlayerName());
+         }
+         else if(checkDiagonals() == false || checkHorizontal() == false || checkVertical() == false) {
+             Players.computerWins();
+             gameWinner('Computer');
+         }
+         else if (Players.getPlayerName.called == false){
+             alert('Enter name before playing!')
+         }
+       }
+     }
+    function playerOneMarks(button) {
+     let mark = document.getElementById(`${button.id}`);
+     mark.classList = 'mark';
+     mark.textContent = 'X'
+     mark.value = 'X';
     }
-    else {
-        if(Players.getPlayerName.called == true) {
-            if(turn){
-                turn = false;
-                playerOneMarks(button), checkHorizontal(), checkDiagonals();
-            }
-           else {
-               turn = true;
-               playerTwoMarks(button), checkHorizontal(), checkDiagonals();
-            }
-        }
-        if(checkHorizontal() == true || checkDiagonals() == true){
-            Players.playerOneWins();
-            gameWinner(Players.getPlayerName());
-        }
-        else if(checkDiagonals() == false || checkHorizontal() == false) {
-            Players.computerWins();
-            gameWinner('Computer');
-        }
-        else if (Players.getPlayerName.called == false){
-            alert('Enter name before playing!')
-        }
-      }
+    function playerTwoMarks(button) {
+     let mark2 = document.getElementById(`${button.id}`);
+     mark2.classList = 'mark2'
+     mark2.textContent = 'O';
+     mark2.value = 'O';
     }
-   function playerOneMarks(button) {
-    let mark = document.getElementById(`${button.id}`);
-    mark.classList = 'mark';
-    mark.textContent = 'X'
-    mark.value = 'X';
-   }
-   function playerTwoMarks(button) {
-    let mark2 = document.getElementById(`${button.id}`);
-    mark2.classList = 'mark2'
-    mark2.textContent = 'O';
-    mark2.value = 'O';
-   }
-   function checkHorizontal() {
-    for (let i = 0; i < 3; i++) {
-        let count = 0;
-        let computerCount = 0;
-        for (let j = i * 3; j < i * 3 + 3; j++) {
-            if(gameButtons[j].value == 'X'){
-                count++;
-                if(count == 3){
-                    return true;
-                }
-            }
-            if(gameButtons[j].value == 'O'){
-                computerCount++
-                if(computerCount == 3){
-                    return false;
-                }
-            }
-            }
-        }
+    function checkHorizontal() {
+     for (let i = 0; i < 3; i++) {
+         let count = 0;
+         let computerCount = 0;
+         for (let j = i * 3; j < i * 3 + 3; j++) {
+             if(gameButtons[j].value == 'X'){
+                 count++;
+                 if(count == 3){
+                     return true;
+                 }
+             }
+             if(gameButtons[j].value == 'O'){
+                 computerCount++
+                 if(computerCount == 3){
+                     return false;
+                 }
+             }
+             }
+         }
+     }
+     function checkVertical() {
+            if(gameButtons[0].value == 'X' && gameButtons[3].value == 'X' && gameButtons[6].value == 'X'){
+                return true
+               }
+               else if(gameButtons[1].value == 'X' && gameButtons[4].value == 'X' && gameButtons[7].value == 'X'){
+                return true
+               }
+               else if(gameButtons[2].value == 'X' && gameButtons[5].value == 'X' && gameButtons[8].value == 'X'){
+                return true
+               }
+               if(gameButtons[0].value == 'O' && gameButtons[3].value == 'O' && gameButtons[6].value == 'O'){
+                return false
+               }
+               else if(gameButtons[1].value == 'O' && gameButtons[4].value == 'O' && gameButtons[7].value == 'O'){
+                return false;
+               }
+               else if(gameButtons[2].value == 'O' && gameButtons[5].value == 'O' && gameButtons[8].value == 'O'){
+                return false;
+               }
     }
-    function checkDiagonals() {
-        if(gameButtons[0].value == 'X' && gameButtons[4].value == 'X' && gameButtons[8].value == 'X') {
-            return true;
-        }
-        else if (gameButtons[2].value == 'X' && gameButtons[4].value == 'X' && gameButtons[6].value == 'X'){
+     function checkDiagonals() {
+         if(gameButtons[0].value == 'X' && gameButtons[4].value == 'X' && gameButtons[8].value == 'X') {
+             return true;
+         }
+         else if (gameButtons[2].value == 'X' && gameButtons[4].value == 'X' && gameButtons[6].value == 'X'){
+             return true;
+         }
+         if(gameButtons[0].value == 'O' && gameButtons[4].value == 'O' && gameButtons[8].value == 'O'){
             return false;
-        }
+         }
+         else if(gameButtons[2].value == 'O' && gameButtons[4].value == 'O' && gameButtons[6].value == 'O'){
+            return false;
+     }
     }
-    function currentPlayer() {
-        if(turn) {
-            return player = 'Computer';
-        }
-        else if (turn == false) {
-            return Players.getPlayerName()
-        }
-    }
-    function gameWinner(winner) {
-       return winningPlayer.innerHTML = `${winner} has won the game!`
-    }
-   return {
-    renderGameboard
-   };
-})();
-const Players = (function () {
-    const playerOne = document.getElementById('player-name-show');
-    const playerTwo = document.querySelector('.player2-name')
-
-    function getPlayerName() {
-        getPlayerName.called = false
-        const firstName = document.getElementById('player-name').value;
-        const input = document.getElementById('enter-name');
-        const button = document.getElementById('show-button');
-        if(firstName.length === 0) {
-            alert('Player name cannot be empty')
-        } else {
-            validateName()
-            let name = document.getElementById('player-name-show').innerHTML = firstName;
-            input.style.display = 'none';
-            button.style.display = 'none';
-            return name;
-        }
-    }
-    function validateName() {
-       return getPlayerName.called = true;
-    }
-    function playerOneWins() {
-        let winCount = 1;
-        const wins = document.getElementById('player-wins');
-        wins.innerHTML = winCount++;
-        return;
-    }
-    function computerWins() {
-        let winCount = 1;
-        const wins = document.getElementById('computer-wins');
-        wins.innerHTML = winCount++;
-        return;
-    }
+     function currentPlayer() {
+         if(turn) {
+             return player = 'Computer';
+         }
+         else if (turn == false) {
+             return Players.getPlayerName()
+         }
+     }
+     function gameWinner(winner) {
+        return winningPlayer.innerHTML = `${winner} has won the game!`
+     }
     return {
-        getPlayerName, playerOneWins, computerWins
+     renderGameboard
     };
-})();
+ })();
+ const Players = (function () {
+     const playerOne = document.getElementById('player-name-show');
+     const playerTwo = document.querySelector('.player2-name')
+ 
+     function getPlayerName() {
+         getPlayerName.called = false
+         const firstName = document.getElementById('player-name').value;
+         const input = document.getElementById('enter-name');
+         const button = document.getElementById('show-button');
+         if(firstName.length === 0) {
+             alert('Player name cannot be empty')
+         } else {
+             validateName()
+             let name = document.getElementById('player-name-show').innerHTML = firstName;
+             input.style.display = 'none';
+             button.style.display = 'none';
+             return name;
+         }
+     }
+     function validateName() {
+        return getPlayerName.called = true;
+     }
+     function playerOneWins() {
+         let winCount = 1;
+         const wins = document.getElementById('player-wins');
+         wins.innerHTML = winCount++;
+         return;
+     }
+     function computerWins() {
+         let winCount = 1;
+         const wins = document.getElementById('computer-wins');
+         wins.innerHTML = winCount++;
+         return;
+     }
+     return {
+         getPlayerName, playerOneWins, computerWins
+     };
+ })();
